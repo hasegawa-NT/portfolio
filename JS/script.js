303ffe82366590a52da03af81dc0699fd46d3ec3
長谷川　直隆 <webcordnn@gmail.com>
2021-06-01 13:47:22 +0900

    header-buttonのUI

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,16 @@
+'use strict';
 $(function() {
+ 
+ $('.header-button').on('click', function() {
+  $('.header-button').toggleClass('js-clicked');
+  $('.gnav').toggleClass('js-show');
+ })
 
     
     
 });
+
+// const headerBtn = document.getElementById('header-button');
+// headerBtn.onclick = function() {
+
+// };
